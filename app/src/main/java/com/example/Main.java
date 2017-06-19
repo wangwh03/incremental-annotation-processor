f34ff8c19eba57a77b48cd84e9e90dab82f2a6f2
Stéphane Nicolas <steff.nicolas@gmail.com>
2017-06-19 10:22:42 -0700

    Trigger annotation processing

---
+++ app/src/main/java/com/example/Main.java
@@ -0,0 +1,8 @@
+package com.example;
+
+@Annotation1
+public class Main {
+  public static void main(String[] args) {
+    System.out.println("foo");
+  }
+}
--- processor1/src/main/java/com/example/Processor1.java
+++ processor1/src/main/java/com/example/Processor1.java
@@ -4,19 +4,30 @@ import java.util.HashSet;
 import java.util.Set;
 import javax.annotation.processing.AbstractProcessor;
 import javax.annotation.processing.RoundEnvironment;
+import javax.lang.model.SourceVersion;
 import javax.lang.model.element.TypeElement;
+
+import static javax.lang.model.SourceVersion.latestSupported;
 
 public class Processor1 extends AbstractProcessor {
 
   @Override
   public Set<String> getSupportedAnnotationTypes() {
     Set<String> set = new HashSet<>();
-    set.add(Annotation1.class.getName());
+    set.add("com.example.Annotation1");
     return set;
   }
 
   @Override
+  public SourceVersion getSupportedSourceVersion() {
+    return latestSupported();
+  }
+
+  @Override
   public boolean process(Set<? extends TypeElement> annotations, RoundEnvironment roundEnv) {
+    for (TypeElement annotation : annotations) {
+      System.out.println("Processing annotation:" + annotation);
+    }
     return false;
   }
 }
